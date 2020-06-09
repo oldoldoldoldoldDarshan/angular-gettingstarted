4a05cc17ae98f7de3a1e05945e529ec72b31dc68
darshanDesktop <darshanvyadav006@gmail.com>
2020-06-09 18:48:54 -0400

    angular routing and angular module info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductListComponent } from './products/product-list.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductListComponent } from './products/product-list.component';
 
 const routes: Routes = [
-    
         {path: 'products', component: ProductListComponent},
-        {path: 'products/:id', component: ProductDetailComponent},
         {path: 'welcome', component: WelcomeComponent},
         {path: '', redirectTo: 'welcome', pathMatch: 'full'},
-        {path: '**', redirectTo: 'welcome', pathMatch: 'full'}           
-
+        {path: '**', redirectTo: 'welcome', pathMatch: 'full'}    
 ];
 
-
+// this will not make router service to entire application - need verification
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatVideoModule} from 'mat-video';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpace } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import {HttpClientModule } from '@angular/common/http';
-import { VideoComponent } from './video/video.component';
-import { ProductDetailComponent } from './products/product-detail.component'
 import { WelcomeComponent } from './home/welcome.component';
-import {RouterModule} from '@angular/router'
-import { AppRoutingModule } from './app-routing.module';
+import { ProductModule } from './products/product.module';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpace,
-    StarComponent,
-    VideoComponent,
-    ProductDetailComponent,
     WelcomeComponent   
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatVideoModule,
-    FormsModule,
     HttpClientModule,
-    RouterModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ]),
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
